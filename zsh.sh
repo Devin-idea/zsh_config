178eabecba77a014640e101d1f5d515121ef5934
Devin <wangle521258@gmail.com>
2022-11-14 19:19:27 +1100

    11

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cat ./.zshrc ~/.zshrc
-chsh -s /bin/zsh
-source .zshrc
+cat ./.zshrc > ~/.zshrc
+#chsh -s /bin/zsh
+source ~/.zshrc
